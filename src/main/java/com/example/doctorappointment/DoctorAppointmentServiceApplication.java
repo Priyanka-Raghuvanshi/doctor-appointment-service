79b5f2a831ea3de09232caedd2a44330ba41c3bb
Priyanka Raghuvanshi <priyanka.raghuvanshi22@stmirascollegepune.edu.in>
2026-09-17 22:26:50 +0530

    Add Doctor Appointment Service project

---
+++ src/main/java/com/example/doctorappointment/DoctorAppointmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.doctorappointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorAppointmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorAppointmentServiceApplication.class, args);
+	}
+
+}
